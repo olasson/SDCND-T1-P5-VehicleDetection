d2f8ca3ed02d7a048b31cf8cc81510526031f6d1
olasson <oystein.lasson@gmail.com>
2021-06-26 21:45:07 +0200

    Added draw bounding boxes

--- code/draw.py
+++ code/draw.py
@@ -4,12 +4,13 @@ import cv2
 import numpy as np
 
 
-def _draw_window(image_tmp, window, color, thickness, fill = False):
+def _draw_rectangle(image_tmp, window, color, thickness, fill = False):
 
     cv2.rectangle(image_tmp, window[0], window[1], color, thickness)
 
     if fill:
-        cv2.rectangle(image_tmp, window[0], window[1], color, -1)
+        fill_color = (color[0] //3, color[1] //3, color[2] //3)
+        cv2.rectangle(image_tmp, window[0], window[1], fill_color, -1)
 
 
 def draw_windows(image, windows, predictions, debug = False):
@@ -20,42 +21,23 @@ def draw_windows(image, windows, predictions, debug = False):
 
         if predictions[i] == 1:
 
-            _draw_window(image_tmp, windows[i], (0, 255, 0), 3, fill = True)
+            _draw_rectangle(image_tmp, windows[i], (0, 255, 0), 3, fill = debug)
         else:
             if debug:
-                _draw_window(image_tmp, windows[i], (0, 0, 100), 1)
+                _draw_rectangle(image_tmp, windows[i], (0, 0, 100), 1)
 
         
     image = cv2.addWeighted(image, 1, image_tmp, 0.7, 0)
 
     return image
 
-
-
-
-"""
-def draw_rectangles(image, rectangles, color, thickness, fill = False):
-
-    #rectangles = np.array(rectangles)
-
-    if len(rectangles) == 0:
-        return image
+def draw_bounding_boxes(image, bounding_boxes, fill = False):
 
     image_tmp = np.zeros_like(image)
 
-    #rectangles = [rectangles]
-
-    for rectangle in rectangles:
-
-        cv2.rectangle(image_tmp, rectangle[0], rectangle[1], color, thickness)
-
-    if fill:
-
-        for rectangle in rectangles:
-
-            cv2.rectangle(image_tmp, rectangle[0], rectangle[1], (0, 50, 0), -1)
+    for bb in bounding_boxes:
+        _draw_rectangle(image_tmp, bb, (255, 0, 0), 5, fill = fill)
 
     image = cv2.addWeighted(image, 1, image_tmp, 0.7, 0)
 
     return image
-"""
