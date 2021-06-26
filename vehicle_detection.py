ea24161dcc808c32d48cf3fbc65547b45e110bea
olasson <oystein.lasson@gmail.com>
2021-06-26 23:35:28 +0200

    Running detector on images

--- vehicle_detection.py
+++ vehicle_detection.py
@@ -1,12 +1,16 @@
 
 import argparse
 
+import numpy as np
+
 from code.config import load_config, config_is_valid
 from code.misc import file_exists, folder_is_empty
 from code.io import glob_images, save_data, load_data
 from code.plots import plot_images
 from code.features import extract_features
 from code.model import prepare_data, svc_train, svc_accuracy
+from code.detect import VehicleDetector
+from code.draw import draw_bounding_boxes
 
 ERROR_PREFIX = 'ERROR_MAIN: '
 INFO_PREFIX = 'INFO_MAIN: '
@@ -36,6 +40,12 @@ if __name__ == "__main__":
         help = 'Folder path to a set of images. Path to a folder containing more folders containing images is valid.',
     )
 
+    parser.add_argument(
+        '--detect',
+        action = 'store_true',
+        help = 'Run the vehicle detector on the set of images from --images.'
+    )
+
     # Show
 
     parser.add_argument(
@@ -54,7 +64,7 @@ if __name__ == "__main__":
     parser.add_argument(
         '--n_max_cols',
         type = int,
-        default = 5,
+        default = 3,
         help = 'The maximum number of columns in the image plot.'
     )
 
@@ -108,6 +118,8 @@ if __name__ == "__main__":
 
     flag_show_images = args.show
 
+    flag_run_on_images = args.detect 
+
     flag_features_are_extracted = file_exists(file_path_features)
     flag_model_exists = file_exists(file_path_model)
 
@@ -115,6 +127,10 @@ if __name__ == "__main__":
 
     if flag_show_images and folder_is_empty(folder_path_images):
         print(ERROR_PREFIX + 'You are trying to show a set of images, but the folder: ' + folder_path_images + ' is empty!')
+        exit()
+
+    if flag_run_on_images and folder_is_empty(folder_path_images):
+        print(ERROR_PREFIX + 'You are trying to run the pipeline on a set of images, but the folder: ' + folder_path_images + ' is empty!')
         exit()
 
     # Show
@@ -131,16 +147,18 @@ if __name__ == "__main__":
 
     # Extract
 
-    if flag_features_are_extracted:
-        print(INFO_PREFIX + 'Loading features!')
-        features_vehicles, features_non_vehicles = load_data(file_path_features, KEY_VEHICLES, KEY_NON_VEHICLES)
-    else:
-        print(INFO_PREFIX + 'Extracting features!')
-        features_vehicles = extract_features(FOLDER_PATH_VEHICLES, config)
-        features_non_vehicles = extract_features(FOLDER_PATH_NON_VEHICLES, config)
+    if not flag_model_exists:
 
-        print(INFO_PREFIX + 'Saving features!')
-        save_data(file_path_features, features_vehicles, features_non_vehicles, KEY_VEHICLES, KEY_NON_VEHICLES)
+        if flag_features_are_extracted:
+            print(INFO_PREFIX + 'Loading features!')
+            features_vehicles, features_non_vehicles = load_data(file_path_features, KEY_VEHICLES, KEY_NON_VEHICLES)
+        else:
+            print(INFO_PREFIX + 'Extracting features!')
+            features_vehicles = extract_features(FOLDER_PATH_VEHICLES, config)
+            features_non_vehicles = extract_features(FOLDER_PATH_NON_VEHICLES, config)
+
+            print(INFO_PREFIX + 'Saving features!')
+            save_data(file_path_features, features_vehicles, features_non_vehicles, KEY_VEHICLES, KEY_NON_VEHICLES)
 
     # Model
 
@@ -159,6 +177,37 @@ if __name__ == "__main__":
 
         print(INFO_PREFIX + 'Saving model!')
         save_data(file_path_model, svc, X_scaler, KEY_SVC, KEY_SCALER)
+
+
+    if flag_run_on_images:
+
+        print(INFO_PREFIX + 'Runing pipeline on images from folder: ' + folder_path_images)
+
+        images = glob_images(folder_path_images, n_max_images = n_max_images)
+
+        n_rows, n_cols, n_channels = images[0].shape
+        
+        n_images = len(images)
+
+        images_result = np.zeros((n_images, n_rows, n_cols, n_channels), dtype = np.uint8)
+        titles = []
+
+        vehicle_detector = VehicleDetector(svc, X_scaler, n_rows, n_cols, config, buffer_size = 0)
+
+        for i in range(n_images):
+
+            bounding_boxes = vehicle_detector.detect(images[i])
+
+            images_result[i] = draw_bounding_boxes(images[i], bounding_boxes, fill = True)
+            titles.append("Number of vehicles detected: " + str(len(bounding_boxes)))
+
+            print(INFO_PREFIX + 'Processed image ' + str(i + 1) + ' of ' + str(n_images) + '!')
+
+        plot_images(images_result, titles, title_fig_window = folder_path_images, n_max_cols = n_max_cols)
+
+
+
+
 
 
 
