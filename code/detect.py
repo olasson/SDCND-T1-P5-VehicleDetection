07bfb6af1fde051be9881fc6697037fb423a2a3e
olasson <oystein.lasson@gmail.com>
2021-06-26 10:33:24 +0200

    Working image slice search

---
+++ code/detect.py
@@ -0,0 +1,58 @@
+
+import cv2
+
+import numpy as np
+
+from code.features import extract_image_features
+
+def _image_slice_search(image, v_min, v_max, h_min, h_max, scale, cells_per_step, config, svc):
+
+    image_slice = image[v_min:v_max, h_min:h_max, :]
+
+    if scale != 1.0:
+
+        if scale > 1.0:
+            interpolation = cv2.INTER_AREA
+        else:
+            interpolation = cv.INTER_LINEAR
+
+        image_slice = cv2.resize(image_slice, (np.int(image_slice.shape[0] / scale), np.int(image_slice.shape[1] / scale)), interpolation = interpolation)
+
+    n_hblocks = (image_slice.shape[1] // config["pix_per_cell"]) - config["cell_per_block"] + 1
+    n_vblocks = (image_slice.shape[0] // config["pix_per_cell"]) - config["cell_per_block"] + 1
+
+    n_blocks_per_window = (config["window"] // config["pix_per_cell"]) - config["cell_per_block"] + 1
+
+
+    h_steps = (n_hblocks - n_blocks_per_window) // cells_per_step + 1
+    v_steps = (n_vblocks - n_blocks_per_window) // cells_per_step + 1
+
+    windows = []
+    predictions = []
+
+    for h_step in range(h_steps):
+        for v_step in range(v_steps):
+            h_pos = h_step * cells_per_step
+            v_pos = v_step * cells_per_step
+
+            window_min_h = h_pos * config["pix_per_cell"]
+            window_min_v = v_pos * config["pix_per_cell"]
+
+            window_image = image_slice[window_min_v:window_min_v + config["window"] , window_min_h:window_min_h + config["window"]]
+
+            features = extract_image_features(window_image, config, 3)
+
+            prediction = svc.predict(features.reshape(1, -1))[0]
+
+            window_scale = np.int(config["window"] * scale)
+
+            top_left = (np.int(window_min_h * scale) + h_min, np.int(window_min_v * scale) + v_min)
+
+            bottom_right = (top_left[0] + window_scale, top_left[1] + window_scale)
+
+            windows.append((top_left, bottom_right))
+
+            predictions.append(prediction)
+
+
+    return windows, predictions
