6e405a34149c1afb130fb38202e42135ac2f066c
olasson <oystein.lasson@gmail.com>
2021-06-26 13:00:48 +0200

    New draw function

--- code/draw.py
+++ code/draw.py
@@ -4,14 +4,46 @@ import cv2
 import numpy as np
 
 
+def _draw_window(image_tmp, window, color, thickness, fill = False):
+
+    cv2.rectangle(image_tmp, window[0], window[1], color, thickness)
+
+    if fill:
+        cv2.rectangle(image_tmp, window[0], window[1], color, -1)
+
+
+def draw_windows(image, windows, predictions, debug = False):
+
+    image_tmp = np.zeros_like(image)
+
+    for i in range(len(windows)):
+
+        if predictions[i] == 1:
+
+            _draw_window(image_tmp, windows[i], (0, 255, 0), 3, fill = True)
+        else:
+            if debug:
+                _draw_window(image_tmp, windows[i], (0, 0, 100), 1)
+
+        
+    image = cv2.addWeighted(image, 1, image_tmp, 0.7, 0)
+
+    return image
+
+
+
+
+"""
 def draw_rectangles(image, rectangles, color, thickness, fill = False):
 
-    rectangles = np.array(rectangles)
+    #rectangles = np.array(rectangles)
 
     if len(rectangles) == 0:
         return image
 
     image_tmp = np.zeros_like(image)
+
+    #rectangles = [rectangles]
 
     for rectangle in rectangles:
 
@@ -26,3 +58,4 @@ def draw_rectangles(image, rectangles, color, thickness, fill = False):
     image = cv2.addWeighted(image, 1, image_tmp, 0.7, 0)
 
     return image
+"""
